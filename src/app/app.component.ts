d57b7dc60bfc0fe105ddc333471e18b88f1b5e55
Edson Cardenas <edson.cardenas@vallegrande.edu.pe>
2021-11-12 12:42:58 -0500

    Primer avanze de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-video-servies';
+export class AppComponent implements OnInit, OnDestroy {
+  intervalSub;
+
+  ngOnInit(): void {
+    this.intervalSub = setInterval(() =>{
+      console.log('Hello from ngOnInit');
+    }, 1000);
+  }
+
+  ngOnDestroy(){
+    if(this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
 }
