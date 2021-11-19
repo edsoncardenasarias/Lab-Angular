b053753e42b2230598f2c0ad8c93bc6c9ccc7b90
Edson Cárdenas Arias <72884555+edsoncardenasarias@users.noreply.github.com>
2021-11-19 11:42:24 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'Aplicación de Calculadora';
+  titulo = 'Calculadora Basica';
   operandoA:number;
   operandoB:number;
   resultado:number;
 
   onSumar():void{
     this.resultado = this.operandoA + this.operandoB;
+  }
+  restar():void{
+    this.resultado = this.operandoA - this.operandoB;
+  }
+  multiplicar():void{
+    this.resultado = this.operandoA * this.operandoB;
+  }
+  dividir():void{
+    this.resultado = this.operandoA / this.operandoB;
   }
 }
